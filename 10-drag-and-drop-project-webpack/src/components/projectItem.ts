0bd99f0fec30954e4b3f0c5906e0b20dcd50b5b9
muhazizal <muhazizal.26@gmail.com>
2022-02-08 21:43:11 +0700

    add webpack to ts project

---
+++ 10-drag-and-drop-project-webpack/src/components/projectItem.ts
@@ -0,0 +1,44 @@
+import { Component } from './baseComponent'
+import { Draggable } from '../interfaces/dragDrop'
+import { Project } from '../models/project'
+import { AutoBind } from '../decorators/autobind'
+export class ProjectItem
+	extends Component<HTMLUListElement, HTMLLIElement>
+	implements Draggable
+{
+	private project: Project
+
+	get getPersons() {
+		return this.project.people === 1
+			? '1 person'
+			: `${this.project.people} persons`
+	}
+
+	constructor(hostId: string, project: Project) {
+		super('single-project', hostId, false, project.id)
+		this.project = project
+		this.configure()
+		this.renderContent()
+	}
+
+	@AutoBind
+	dragStartHandler(event: DragEvent) {
+		event.dataTransfer!.setData('text/plain', this.project.id)
+		event.dataTransfer!.effectAllowed = 'move'
+	}
+
+	dragEndHandler(_event: DragEvent) {}
+
+	configure() {
+		this.element.addEventListener('dragstart', this.dragStartHandler)
+		this.element.addEventListener('dragend', this.dragEndHandler)
+	}
+
+	renderContent() {
+		this.element.querySelector('h2')!.textContent = this.project.title
+		this.element.querySelector(
+			'h3'
+		)!.textContent = `${this.getPersons} assigned`
+		this.element.querySelector('p')!.textContent = this.project.description
+	}
+}
---
+++ 10-drag-and-drop-project-webpack/src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+export function AutoBind(
+	_target: any,
+	_name: string,
+	descriptor: PropertyDescriptor
+) {
+	const adjacentDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			return descriptor.value.bind(this)
+		},
+	}
+	return adjacentDescriptor
+}
